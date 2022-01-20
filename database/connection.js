3474c51507232308b94bcb6de3df60d51ac922a1
runzw <runzw@students.cs.ubc.ca>
2022-01-20 01:19:44 -0800

    db connection

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/inventory', {
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@cluster0.ovnsr.mongodb.net/inventory?authSource=admin&replicaSet=atlas-qlrdyu-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', {
     useNewUrlParser: true
 })
 
